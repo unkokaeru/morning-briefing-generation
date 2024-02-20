35fb75e5c829cc814b2a151347b7e2cb5fff16d9
William F <110934475+unkokaeru@users.noreply.github.com>
2024-02-20 16:25:29 +0000

    Added a requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """A script to generate a morning briefing in a markdown document."""
+
 from template_rendering.markdown_generator import doc_gen
 from utils.logger import get_logger
 
 # TODO: Add tests with pytest and pytest-cov (see tests/morning_briefing_tests.py), and add a CI/CD pipeline with GitHub Actions
 # TODO: Add asynchronicity to the script (asyncio, httpx, asyncio TaskGroup)
-# TODO: Call my phone and play the mp3 file (generated with GPT-4 and TTS) with a phone API (Twilio?)
+# TODO: Call my phone and play the mp3 file (generated with GPT-4 and ElevenLabs) with a phone API (Twilio?)
 # TODO: Add a GUI (PySimpleGUI? Streamlit? Tkinter? PyQt?): customise what is included in the briefing, and when it is generated
 
 logger = get_logger()  # Initialize the logger
